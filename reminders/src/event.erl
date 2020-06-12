68df39d9e44d2b7664414a2f4d75d161a41da4ef
Maciej Warchalowski <maciej.warchalowski@wellspring.com>
2020-06-12 17:02:37 -0500

    Adding interface to events

--- reminders/src/event.erl
+++ reminders/src/event.erl
@@ -4,6 +4,28 @@
                 name="",
                 to_go=0}).
 
+start(EventName, Delay) ->
+  spawn(?MODULE, init, [self(), EventName, Delay]).
+
+start_link(EventName, Delay) ->
+  spawn_link(?MODULE, init, [self(), EventName, Delay]).
+
+init(Server, EventName, Delay) ->
+  loop(#state{server=Server,
+              name=EventName,
+              to_go=normalize(Delay)}).
+
+cancel(Pid) ->
+  Ref = erlang:monitor(process, Pid), % In case process is dead.
+  Pid ! {self(), Ref, cancel},
+  receive
+    {Ref, ok} ->
+      erlang:demonitor(Ref, [flush]),
+      ok;
+    {'DOWN', Ref, process, Pid, _Reason} ->
+      ok
+  end.
+
 loop(S = #state{server = Server, to_go = [T|Next]}) ->
   receive
     {Server, Ref, cancel} ->
