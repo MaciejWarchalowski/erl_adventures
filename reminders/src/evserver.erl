ab467dffb411df9f45e60b5deb92dba7f7884e92
Maciej Warchalowski <maciej.warchalowski@wellspring.com>
2020-06-15 02:50:31 -0500

    Events server

--- reminders/src/evserver.erl
+++ reminders/src/evserver.erl
@@ -14,10 +14,6 @@
   timeout = {{1970,1,1},{0,0,0}}
 }).
 
-init() ->
-  loop(#state{events = orddic:new(),
-              clients = orddic:new()}).
-
 loop(S = #state{clients = Clients, events = Events}) ->
   receive
     {From, Ref, {subscribe, Client}} ->
@@ -59,18 +55,18 @@ loop(S = #state{clients = Clients, events = Events}) ->
           loop(S)
       end;
     shutdown ->
-      ok;
+      exit(shutdown);
     code_change ->
-      ok;
+      ?MODULE:loop(S);
     {'DOWN', Ref, process, _Reason} ->
-      ok;
+      loop(S#state{clients=orddict:erase(Ref, Clients)});
     Unknown ->
       io:format("Unknown message: ~p ~n", [Unknown]),
       loop(S)
   end.
 
 send_to_clients(Msg, Clients) ->
-  error.
+  orddic:map(fun(_Ref, Pid) -> Pid ! Msg end, Clients).
 
 valid_datetime({Date, Time}) ->
   try
@@ -85,3 +81,54 @@ valid_time({H, M, S}) when H >= 0, H < 24,
                            M >= 0, M < 60,
                            S >= 0, S < 60 -> true;
 valid_time(_) -> false.
+
+start() ->
+  register(?MODULE, Pid = spawn(?MODULE, init, [])),
+  Pid.
+
+start_link() ->
+  register(?MODULE, Pid = spawn_link(?MODULE, init, [])),
+  Pid.
+
+init() ->
+  loop(#state{events = orddic:new(),
+              clients = orddic:new()}).
+
+terminate() ->
+  ?MODULE ! shutdown.
+
+subscribe(Pid) ->
+  Ref = erlang:monitor(process, whereis(?MODULE)),
+  ?MODULE ! {self(), Ref, {subscribe, Pid}},
+  receive
+    {Ref, ok} -> {ok, Ref};
+    {'DOWN', Ref, process, _Pid, Reason} -> {error, Reason}
+  after 5000 ->
+    {error, timeout}
+  end.
+
+add_event(Name, Description, DateTime) ->
+  Ref = make_ref(),
+  ?MODULE ! {self(), Ref, {add, Name, Description, DateTime}},
+  receive
+    {Ref, Msg} -> Msg
+  after 5000 ->
+    {error, timeout}
+  end.
+
+cancel(Name) ->
+  Ref = make_ref(),
+  ?MODULE ! {self(), Ref, {cancel, Name}},
+  receive
+    {Ref, ok} -> ok
+  after 5000 ->
+    {error, timeout}
+  end.
+
+listen(Delay) ->
+  receive
+    E = {done, _Name, _Description} ->
+      [E|listen(0)]
+  after Delay * 1000 ->
+    []
+  end.
